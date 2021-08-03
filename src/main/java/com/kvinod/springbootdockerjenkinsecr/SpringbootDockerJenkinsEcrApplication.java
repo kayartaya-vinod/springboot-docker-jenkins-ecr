411571e2c4c987a2df8e538d99917d2f161dde4d
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-08-03 10:57:19 +0530

    first commit

---
+++ src/main/java/com/kvinod/springbootdockerjenkinsecr/SpringbootDockerJenkinsEcrApplication.java
@@ -0,0 +1,13 @@
+package com.kvinod.springbootdockerjenkinsecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerJenkinsEcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerJenkinsEcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kvinod/springbootdockerjenkinsecr/SpringbootDockerJenkinsEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvinod.springbootdockerjenkinsecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerJenkinsEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
